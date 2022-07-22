b419393b85052f5d5bbdd2780a5d9663739c062b
VuDej <drvu47@gmail.com>
2022-07-22 08:35:35 +0200

    crerated react app

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('callEnded');
   });
 
-  socket.on('callUser', ({
-    userToCall, signalData, from, name,
-}) => {
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
     io.to(userToCall).emit('callUser', { signal: signalData, from, name });
   });
 
