e113c2a1158f8a36b1d04c9771faffdb21e57d4f
VuDej <drvu47@gmail.com>
2022-07-22 11:36:43 +0200

    redy for deploy

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('callEnded');
   });
 
-  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+  socket.on('callUser', ({
+    userToCall, signalData, from, name
+}) => {
     io.to(userToCall).emit('callUser', { signal: signalData, from, name });
   });
 
